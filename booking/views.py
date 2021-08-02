7dbd54243d0adc16e9fa4290adfe4041c4e9281b
Kailasvs94 <47276554+Kailas143@users.noreply.github.com>
2021-08-02 15:22:49 +0530

    Update views.py

--- booking/views.py
+++ booking/views.py
@@ -17,25 +17,25 @@ def findbus(request):
         src_r = request.POST.get('source')
         dest_r = request.POST.get('dest')
         date_r = request.POST.get('date')
-        bus_list = Bus.objects.filter(src=src_r, dest=dest_r, date=date_r)
+        bus_list = Bus.objects.filter(src=src_r, dest=dest_r, date=date_r)#filter the bus based on the details given by user
         if bus_list:
-            return render(request, 'busapp/buslist.html', locals())
+            return render(request, 'busapp/buslist.html', locals())#provide the filtered bus result in Html page
         else:
-            context["error"] = "Sorry no buses availiable"
-            return render(request, 'busapp/findbus.html', context)
+            context["error"] = "Sorry no buses availiable" #if bus is not available the this this will shown in page
+            return render(request, 'busapp/findbus.html', context)#context is passing to the page
     else:
         return render(request, 'busapp/findbus.html')
 
 
-
+#booking section
 def booking_list(request):
     context = {}
     if request.method == "POST":
         bus_id = request.POST.get('bus_id')
         no_s = request.POST.get('no_seats')
-        bus = get_object_or_404(Bus, id=bus_id)
+        bus = get_object_or_404(Bus, id=bus_id)# get the bus based on the id given by the user
         if bus:
-            if bus.rems > int(no_s):
+            if bus.rems > int(no_s): #checking condition
                 bus_name = bus.bus_name
                 src_r = bus.src
                 dest_r = bus.dest
@@ -43,12 +43,13 @@ def booking_list(request):
                 time_r = bus.time
                 price_r = bus.price
                 nos = no_s
-                cost = bus.price * int(no_s)
-                user_name = request.user.username
-                user_email = request.user.email
-                user_id = request.user.id
-                rem_r = bus.rems-int(no_s)
-                Bus.objects.filter(id=bus_id).update(rems=rem_r)
+                cost = bus.price * int(no_s) #perform addition to find the cost of the ticket(no_s given by the user)
+                user_name = request.user.username #authenticated user name
+                user_email = request.user.email #authenticated user email
+                user_id = request.user.id 
+                rem_r = bus.rems-int(no_s) 
+                Bus.objects.filter(id=bus_id).update(rems=rem_r) #updating the remaining seat field of the particular bus
+                #storing booking detailes to the table or model Book
                 booking = Book.objects.create(
                     busid=bus_id,
                     bus_name=bus_name,
@@ -70,7 +71,7 @@ def booking_list(request):
             return render(request, 'busapp/findbus.html', context)
     return render(request, 'busapp/findbus.html')
 
-
+#to list or show the booking details or history 
 def seebookings(request, new={}):
     context = {}
     id_r = request.user.id
@@ -81,23 +82,23 @@ def seebookings(request, new={}):
         context['error'] = 'Sorry no bookings'
         return render(request, 'busapp/findbus.html', context)
 
-
+#cancelling function
 def cancelling(request):
     context = {}
     if request.method == 'POST':
-        id_r = request.POST.get('book_id')
+        id_r = request.POST.get('book_id')#get id of the booking
         try:
-            book = Book.objects.get(id=id_r)
+            book = Book.objects.get(id=id_r)#access the book based on the id given by computer
             print(book)
-            bus = Bus.objects.get(id=book.busid)
+            bus = Bus.objects.get(id=book.busid)# get the bus based on the busid from the book table
             print(book.nos)
             print(bus.rems)
-            rem_r = bus.rems+int(book.nos)
-            Bus.objects.filter(id=book.busid).update(rems=rem_r)
+            rem_r = bus.rems+int(book.nos) #updation the rems field (after cancellation the rems should be increased
+            Bus.objects.filter(id=book.busid).update(rems=rem_r) # updating the remaining seats
             print(bus.rems)
-            Book.objects.filter(id=id_r).update(status='CANCELLED')
+            Book.objects.filter(id=id_r).update(status='CANCELLED')#updation the status of the book by cancelling
             Book.objects.filter(id=id_r).update(nos=0)
-            return redirect('booking:seebookings')
+            return redirect('booking:seebookings')#eredirecting the url
         except Book.DoesNotExist:
             context['error'] = 'Invalid Booking ID'
             return render(request, 'busapp/error.html', context)
